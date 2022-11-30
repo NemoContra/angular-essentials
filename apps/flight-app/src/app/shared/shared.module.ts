959f041e4f15dc1d9ae3a4e3b1a6efef85f4f4d3
Florian Tischler <Florian.Tischler@gmx.net>
2022-11-30 12:22:11 +0100

    add solution with modules

--- apps/flight-app/src/app/shared/shared.module.ts
+++ apps/flight-app/src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { AsyncCityPipe } from './city.pipe';
+import { AsyncCityPipe } from './async-city.pipe';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
